6bf834f4390963248918a4cc40af7793c519ad4b
Jinho Heo <uj07077@gmail.com>
2024-03-28 19:33:53 +0900

    feat: example_8_4 add ProcessPoolExecutor

--- RL_an_introduction/ch08/example_8_4.py
+++ RL_an_introduction/ch08/example_8_4.py
@@ -5,6 +5,8 @@
 # declaration at the top                                              #
 #######################################################################
 
+from concurrent.futures import ProcessPoolExecutor
+
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
@@ -12,6 +14,29 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 from maze import *
+
+def run_experiment(run, method_names, models, methods, params, mazes, num_of_mazes, params_dyna):
+    backups = np.zeros((2, num_of_mazes))
+    for i in range(len(method_names)):
+        for mazeIndex, maze in enumerate(mazes):
+            print(f'run {run}, {method_names[i]}, maze size {maze.WORLD_HEIGHT * maze.WORLD_WIDTH}')
+            
+            q_value = np.zeros(maze.q_size)
+            steps = []
+            model = models[i]()
+            
+            while True:
+                steps.append(methods[i](q_value, model, maze, params[i]))
+                
+                if check_path(q_value, maze):
+                    break
+            
+            backups[i, mazeIndex] = np.sum(steps)
+    
+    # Dyna-Q performs several backups per step
+    backups[1, :] *= params_dyna.planning_steps + 1
+    return backups
+
 
 def example_8_4():
     original_maze = Maze()
@@ -71,6 +96,55 @@ def example_8_4():
 
     plt.savefig('./plots/example_8_4.png')
     plt.close()             
-                
+
+def example_8_4_parallel():
+    original_maze = Maze()
+    
+    params_prioritized = DynaParams()
+    params_prioritized.theta = 0.001
+    params_prioritized.planning_steps = 5
+    params_prioritized.alpha = 0.5
+    params_prioritized.gamma = 0.95
+    
+    params_dyna = DynaParams()
+    params_dyna.planning_steps = 5
+    params_dyna.alpha = 0.5
+    params_dyna.gamma = 0.95
+    
+    params = [params_prioritized, params_dyna]
+    models = [PriorityModel, TrivalModel]
+    method_names = ['Prioritized Sweeping', 'Dyna-Q']
+    
+    num_of_mazes = 5
+    
+    mazes = [original_maze.extend_maze(i) for i in range(1, num_of_mazes + 1)]
+    methods = [prioritized_sweeping, dyna_q]
+    
+    runs = 5
+    all_backups = []
+
+    # Use ProcessPoolExecutor to parallelize the runs
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(run_experiment, run, method_names, models, methods, params, mazes, num_of_mazes, params_dyna) for run in range(runs)]
+        for future in futures:
+            all_backups.append(future.result())
+
+    # Compute the average over all runs
+    backups = np.mean(all_backups, axis=0)
+
+    # Dyna-Q performs several backups per step
+    backups[1, :] *= params_dyna.planning_steps + 1
+
+    for i in range(0, len(method_names)):
+        plt.plot(np.arange(1, num_of_mazes + 1), backups[i, :], label=method_names[i])
+    plt.xlabel('maze resolution factor')
+    plt.ylabel('backups until optimal solution')
+    plt.yscale('log')
+    plt.legend()
+
+    plt.savefig('./plots/example_8_4.png')
+    plt.close()             
+
+
 if __name__ == '__main__':
-    example_8_4()        
+    example_8_4_parallel()        
