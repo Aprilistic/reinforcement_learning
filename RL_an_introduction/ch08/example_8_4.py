cc163552afaa0cd7cf0d200601ace01cc311a3b2
Jinho Heo <uj07077@gmail.com>
2024-03-28 15:34:13 +0900

    sytle: add copyright

--- RL_an_introduction/ch08/example_8_4.py
+++ RL_an_introduction/ch08/example_8_4.py
@@ -1,3 +1,10 @@
+#######################################################################
+# Copyright (C)                                                       #
+# 2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
---
+++ RL_an_introduction/ch08/figure_8_7.py
@@ -0,0 +1,7 @@
+#######################################################################
+# Copyright (C)                                                       #
+# 2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
